7d344321ca22bca55a11b6ed37f878fbe3b77330
keidaroo <kei.asano.kei@gmail.com>
2019-03-29 16:21:06 +0900

    fixed burger

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ $(document).ready(function () {
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
+    $(".navbar-link").css("border-color", "#ffff");
+    // $(".navbar-link").toggleClass("is-active");
 
   });
 });
